4322d2e01adf35dda7dc6d87c496b8216f750a1c
daloopy <dvaquera@purdue.edu>
2023-04-05 20:27:51 -0400

    cleaned up frontend form; only accept file upload

--- website/react/src/index.js
+++ website/react/src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import PackageList from './components/PackageList';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App>
-      <PackageList />
     </App>
   </React.StrictMode>
 );
