c4ac7708b31c284f79998503e76282d14b6d32ca
daloopy <dvaquera@purdue.edu>
2023-04-06 20:57:13 -0400

    switching to main

--- website/react/src/components/PackageForm.js
+++ website/react/src/components/PackageForm.js
@@ -1,23 +1,15 @@
 import React, { useState } from 'react';
 
 function PackageForm() {
-  //const [ownerName, setOwnerName] = useState(''); // initialize var to empty string
-  //const [repoName, setRepoName] = useState('');
-  //const [URL, setUrl] = useState('');
-  //const [scores, setScores] = useState('');
-  const  [zipfile, setFile] = useState('');
+  const  [zipfile, setFile] = useState(''); // 
 
   const handleSubmit = async (event) => {
     event.preventDefault();
   
     const packageData = new FormData();
-    //packageData.append('owner_name', ownerName);
-    //packageData.append('repository_name', repoName);
-    //packageData.append('url', URL);
-    //packageData.append('scores', scores.split(','));
     packageData.append('file', zipfile);
   
-    const response = await fetch('/api/packages', { // use fetch API to make a POST request to the endpoint
+    const response = await fetch('/api/packages', { // use fetch API to make a POST request to /api/packages endpoint
       method: 'POST',
       body: packageData,
     });
@@ -29,6 +21,7 @@ function PackageForm() {
     }
   };
 
+  // callback for when file is uploaded
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
   }
@@ -36,7 +29,7 @@ function PackageForm() {
   return (
     <form onSubmit={handleSubmit}>
       <div>
-        <label htmlFor="file">Choose a file:</label>
+        <label htmlFor="file">Choose a zipfile:</label>
         <input type="file" id="file" name="file" onChange={handleFileChange} />
       </div>
       <button type="submit">Create Package</button>
