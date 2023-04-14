093d3d68fc38ccfb5ead6a799e8c574f7b9a7986
daloopy <dvaquera@purdue.edu>
2023-04-14 12:19:59 -0400

    using homepage for git url

--- website/backend/parser.js
+++ website/backend/parser.js
@@ -1,14 +1,29 @@
 const fs = require('fs');
 
 function getUrlFromPackageJson() {
+  /* https://piazza.com/class/lc9qx1w86k643t/post/230 
+    assume the homepage URL is the github repository
+  */
+
   // check for package.json; if it doesnt exist return bad request
 
   // open the package.json file for parsing
   const packageJson = fs.readFileSync('package.json'); // package.json is in same directory as this file
   const data = JSON.parse(packageJson);
 
-
   // check if repository field & repository url field (ex. browsify, cloudinary)
+  if (data.homepage) {
+    let url = data.homepage;
+    if (url.includes('github.com')) {
+      return url;
+    }
+  }
+  return undefined;
+}
+
+
+/*
+// check if repository field & repository url field (ex. browsify, cloudinary)
   if (data.repository && data.repository.url) {
     let url = data.repository.url;
     if (url.endsWith('.git')) {
@@ -22,10 +37,4 @@ function getUrlFromPackageJson() {
     return url;
   }
 
-
-  return undefined;
-}
-
-function verifyOwnerRepo(){
-  return 1;
-}
+*/
