4a5a9c331d7d8ebd17b8282a532fbe8aab6b489a
WahabAkanbi <wakanbi@purdue.edu>
2023-04-19 23:47:06 -0400

    errors still

--- packageDirectory/database.py
+++ packageDirectory/database.py
@@ -26,6 +26,27 @@ class MyTable(Base):
     version_score = Column(Float)
     adherence_score = Column(Float)
 '''
+Base = declarative_base()
+class MyTable(Base):
+    __tablename__ = 'my_table'
+    id = Column(Integer, primary_key=True)
+    repo_name = Column(String(255))
+    url = Column(String(255))
+    total_score = Column(Float)
+    ramp_up_score = Column(Float)
+    correctness_score = Column(Float)
+    bus_factor = Column(Float)
+    responsiveness_score = Column(Float)
+    license_score = Column(Float)
+    version_score = Column(Float)
+    adherence_score = Column(Float)
+'''    
+class MyZipFilesTable(Base):
+    __tablename__ = 'my_zip_files_table'
+    id = Column(Integer, primary_key=True)#auto generated to store the row
+    file_identifier = Column(String(255)) #name of the file
+    zipped_file = Column(LargeBinary)    #binary version of the file
+'''    
 
 def getconn() -> pymysql.connections.Connection:
     conn: pymysql.connections.Connection = connector.connect(
@@ -44,25 +65,6 @@ def create_engine_with_conn_pool() -> sqlalchemy.engine.Engine:
     return engine
 
 def create_table(engine: sqlalchemy.engine.Engine):
-    Base = declarative_base()
-    class MyTable(Base):
-        __tablename__ = 'my_table'
-        id = Column(Integer, primary_key=True)
-        repo_name = Column(String(255))
-        url = Column(String(255))
-        total_score = Column(Float)
-        ramp_up_score = Column(Float)
-        correctness_score = Column(Float)
-        bus_factor = Column(Float)
-        responsiveness_score = Column(Float)
-        license_score = Column(Float)
-        version_score = Column(Float)
-        adherence_score = Column(Float)
-    class MyZipFilesTable(Base):
-        __tablename__ = 'my_zip_files_table'
-        id = Column(Integer, primary_key=True)#auto generated to store the row
-        file_identifier = Column(String(255)) #name of the file
-        zipped_file = Column(LargeBinary)    #binary version of the file
     Base.metadata.create_all(bind=engine)
 
 def insert_data(engine: sqlalchemy.engine.Engine, data):
@@ -75,6 +77,66 @@ def insert_data(engine: sqlalchemy.engine.Engine, data):
     except Exception as e:
         print(f"Error occurred while inserting data: {e}")
 
+
+def retrieve_data(engine: sqlalchemy.engine.Engine):
+    try:
+        # interact with Cloud SQL database using connection pool
+        with engine.connect() as db_conn:
+            # query database
+            result = db_conn.execute(MyTable.__table__.select()).fetchall()
+            # return results
+            return result
+    except Exception as e:
+        print(f"Error occurred while retrieving data: {e}")
+
+def defineData(id, Rname, url, total_score, ramp_score, cor_score, bus_factor, res_score, lisc_score, version_score, adhere_score):
+    # define data to be inserted
+    data = [{"id": id, "repo_name": Rname, "url": url,
+             "total_score": total_score, "ramp_up_score": ramp_score, "correctness_score": cor_score,
+             "bus_factor": bus_factor, "responsiveness_score": res_score, "license_score": lisc_score,
+             "version_score": version_score, "adherence_score": adhere_score}]
+    return data
+
+def printOutput(results):
+    # print results
+    print(results)
+    '''
+    for row in results:
+        print(row)
+    '''   
+
+def retrieve_table_description(engine: sqlalchemy.engine.Engine, table_name: str):
+    try:
+        # interact with Cloud SQL database using connection pool
+        with engine.connect() as db_conn:
+            # query table description
+            query = f"SELECT * FROM information_schema.columns WHERE table_name = '{table_name}'"
+            result = db_conn.execute(query).fetchall()
+            return result
+    except Exception as e:
+        print(f"Error occurred while retrieving table description: {e}")    
+
+def main():
+    # create connection pool
+    engine = create_engine_with_conn_pool()
+    create_table(engine)
+    data = defineData(1, "repo1","http://example.com/repo1",
+    0.8, 0.7, 0.9, 0.6, 0.85, 0.9, 0.75, 0.8)
+
+    table_description = retrieve_table_description(engine, 'MyTable')
+    printOutput(table_description)
+    # insert data into database
+    #insert_data(engine, data)
+    # retrieve data from database
+    #results = retrieve_data(engine)
+    printOutput(table_description)
+
+if __name__ == '__main__':
+    # initialize Connector object
+    connector = Connector()
+    main()
+    connector.close()
+'''
 #assumes the data is downloaded into the computer first and the file path is then sent to this function
 def insert_zipped_file(engine: sqlalchemy.engine.Engine, file_path: str, file_identifier: str):
     try:
@@ -111,45 +173,4 @@ def retrieve_zipped_file_by_id(engine: sqlalchemy.engine.Engine, file_id: int):
                 return None
     except Exception as e:
         print(f"Error occurred while retrieving data: {e}")
-
-def retrieve_data(engine: sqlalchemy.engine.Engine):
-    try:
-        # interact with Cloud SQL database using connection pool
-        with engine.connect() as db_conn:
-            # query database
-            result = db_conn.execute(MyTable.__table__.select()).fetchall()
-            # return results
-            return result
-    except Exception as e:
-        print(f"Error occurred while retrieving data: {e}")
-
-def defineData(id, Rname, url, total_score, ramp_score, cor_score, bus_factor, res_score, lisc_score, version_score, adhere_score):
-    # define data to be inserted
-    data = {"id": id, "repo_name": Rname, "url": url,
-        "total_score": total_score, "ramp_up_score": ramp_score, "correctness_score": cor_score,
-        "bus_factor": bus_factor, "responsiveness_score": res_score, "license_score": lisc_score,
-        "version_score": version_score, "adherence_score": adhere_score}
-    return data
-
-def printOutput(results):
-    # print results
-    for row in results:
-        print(row)
-
-def main():
-    # create connection pool
-    engine = create_engine_with_conn_pool()
-    create_table(engine)
-    data = defineData(1, "repo1","http://example.com/repo1",
-    0.8, 0.7, 0.9, 0.6, 0.85, 0.9, 0.75, 0.8)
-    # insert data into database
-    insert_data(engine, data)
-    # retrieve data from database
-    results = retrieve_data(engine)
-    printOutput(results)
-
-if __name__ == '__main__':
-    # initialize Connector object
-    connector = Connector()
-    main()
-    connector.close()
+'''            
