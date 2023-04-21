14b11ca37b8a7158728d5862832662b8c064e159
WahabAkanbi <wakanbi@purdue.edu>
2023-04-20 20:40:19 -0400

    all done with 2 tables, can also download zipped file

--- packageDirectory/database.py
+++ packageDirectory/database.py
@@ -2,7 +2,10 @@ import pymysql
 from google.cloud.sql.connector import Connector
 import sqlalchemy
 from sqlalchemy import create_engine, Column, Integer, String, Float,LargeBinary, pool, text, exc
-
+import zipfile
+from io import BytesIO
+import requests
+import os
 
 def getconn() -> pymysql.connections.Connection:
     conn: pymysql.connections.Connection = Connector().connect(
@@ -58,7 +61,7 @@ def create_zip_table(engine):
             id INT AUTO_INCREMENT PRIMARY KEY,
             file_id VARCHAR(255),
             url VARCHAR(255),
-            zipped_file LONGBLOB,
+            zipped_file LONGBLOB
             );
         """)
         conn.execute(stmt)
@@ -83,7 +86,7 @@ def insert_zipped_data(engine, file_id, url, zipped_file):
 def retrieve_zipped_file(engine, url):
     with engine.connect() as conn:
         result = conn.execute(
-            text("SELECT zipped_file FROM zip_files WHERE url=:url"), {"url": url}
+            text("SELECT zipped_file FROM zipped_table WHERE url=:url"), {"url": url}
         ).fetchone()
         if result is not None:
             zipped_file = result[0]
@@ -130,33 +133,6 @@ def insert_repo_data(engine, repo_name, url, total_score, ramp_up_score, correct
             print("Data inserted successfully.")
         except exc.SQLAlchemyError as e:
             print(f"Error occurred: {e}")
-'''
-def insert_data(engine,row, repo_name, url, total_score, ramp_up_score, correctness_score,
-                bus_factor, responsiveness_score, license_score, version_score, adherence_score):
-    with engine.connect() as conn:
-        # Check if a record with the same URL already exists
-        insert_stmt = sqlalchemy.text(
-            "INSERT INTO repo_info (id, repo_name, url, total_score, ramp_up_score, correctness_score, bus_factor, responsiveness_score, license_score, version_score, adherence_score) VALUES (:id, :repo_name, :url, :total_score, :ramp_up_score, :correctness_score, :bus_factor, :responsiveness_score, :license_score, :version_score, :adherence_score)"
-        )
-        try:
-            conn.execute(insert_stmt, parameters= {"id": row,
-                "repo_name": repo_name,
-                "url": url,
-                "total_score": total_score,
-                "ramp_up_score": ramp_up_score,
-                "correctness_score": correctness_score,
-                "bus_factor": bus_factor,
-                "responsiveness_score": responsiveness_score,
-                "license_score": license_score,
-                "version_score": version_score,
-                "adherence_score": adherence_score,
-            })
-            result = conn.execute(sqlalchemy.text("SELECT * from repo_info")).fetchall()
-            conn.commit()
-
-        except exc.SQLAlchemyError as e:
-            print(f"Error occurred: {e}")
-'''
 #this retrieve function retrieves entire table
 
 def retrieve_repo_data(engine):
@@ -171,37 +147,73 @@ def retrieve_repo_data(engine):
 def retrieve_repo_data_url(engine, url):
     with engine.connect() as conn:
         data = conn.execute(sqlalchemy.text("SELECT * FROM repo_info WHERE url=:url"), {"url": url}).fetchall()
-        print(data)
+        #print(data)
         if len(data) > 0:
             return data[0]
         else:
             return None
 
+def download_zipped_file(zipped_file, file_name, download_path):
+    if not os.path.exists(download_path):
+        os.makedirs(download_path)
+
+    file_path = os.path.join(download_path, file_name)
+
+    with open(file_path, 'wb') as f:
+        f.write(zipped_file)
+
+    print(f"Zipped file downloaded and saved to {file_path}")
+
+
+
 def main():
     # Establish a connection to the database
     engine = create_engine_with_conn_pool()
-    #delete_table(engine, "repo_info")
-    #create_repo_table(engine)
-    #delete_table(engine, "repo_info")
-    #print("table created")
-    #insert_repo_data(engine, "my_repo", "https://github.com/my_repo", 0.8, 0.7, 0.9, 0.6, 0.85, 0.9, 0.75, 0.8)  
+    #you can only have one table per name
+
+    #want to retrieve entire table use retrieve_repo_data function else if u want to use url use retrieve_repo_data_url
+
+    # how to create repo info table
+    '''
+    delete_table(engine, "repo_info")
+    create_repo_table(engine)
+    insert_repo_data(engine, "my_repo", "https://github.com/my_repo", 0.8, 0.7, 0.9, 0.6, 0.85, 0.9, 0.75, 0.8)  
 
     data = retrieve_repo_data_url(engine,"https://github.com/my_repo")
     if data:
         print(data)
     else:
         print("No matching row found.")
+    '''
 
-    #create_zip_table(engine)
+    #process for zipped file
+    '''
+    delete_table(engine, "zipped_table")
+    create_zip_table(engine)
+    
+    with open("./temp.zip", "rb") as f:
+        zip_content = f.read()
+    insert_zipped_data(engine, "example.zip", "https://example.com/example.zip", zip_content)
+    zipped_data = retrieve_zipped_file(engine, "https://example.com/example.zip")
+    # unzip the file and check contents
+    with BytesIO(zipped_data) as zip_data:
+        with zipfile.ZipFile(zip_data) as myzip:
+            # do something with the unzipped files
+            file_names = myzip.namelist()
+            print(file_names)
 
-      
-        
-        
-        
-        
-      
-        
+    #assuming you want to download file content
+    file_name = "temp.zip"
+    download_path = "/home/shay/a/wakanbi/VJAM/packageDirectory" 
+    if zipped_data is not None:
+        download_zipped_file(zipped_data, file_name, download_path)
+    '''    
 
+
+    #authethentication- put this in terminal before running python3 database.py
+    #export GOOGLE_APPLICATION_CREDENTIALS="/home/shay/a/wakanbi/VJAM/packageDirectory/serviceKey.json" 
+        
+    
 
 if __name__ == '__main__':
     main()
@@ -219,7 +231,6 @@ from sqlalchemy import create_engine, Column, Integer, String, Float,LargeBinary
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Set up the Cloud SQL connection
-#export GOOGLE_APPLICATION_CREDENTIALS="/home/shay/a/wakanbi/VJAM/packageDirectory/serviceKey.json" 
 
 class MyTable(Base):
     __tablename__ = 'my_table'
