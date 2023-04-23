6e96f07efeb197eee971c243420289d0c0f52f1f
Dalilah Vaquera <70181295+daloopy@users.noreply.github.com>
2023-04-22 22:25:25 -0500

    61 fix gcp (#70)
    * fix gcp.yml
    
    * change routing
    
    * check for previous instance before stopping
    
    * removed openapi_spec
    
    * removed version number
    
    * specified service account email
    
    * changed fetch url

--- website/react/src/App.js
+++ website/react/src/App.js
@@ -5,7 +5,7 @@ import PackageForm from './components/PackageForm';
 
 function App(prop) {
   const [message, setMessage] = useState("");
-  const apiUrl = 'https://ece-461-part-2-web-service.uk.r.appspot.com/api'
+  const apiUrl = 'https://backend-service-dot-ece-461-part-2-web-service.uk.r.appspot.com/'
   useEffect(() => {
     fetch(apiUrl)
       .then((res) => res.json())
