c13cfae3eb82cb1b7defe607d887e216c8b4219f
WahabAkanbi <wakanbi@purdue.edu>
2023-05-02 19:10:48 -0400

    ALL DATABASE WORKS

--- packageDirectory/database.js
+++ packageDirectory/database.js
@@ -32,27 +32,9 @@ const pool = mysql.createPool({
     queueLimit: 0
   }).promise();
 
-  /*
-  const result = await pool.query("SELECT * FROM repo_info");
-  const row = result[0][0];
-  console.log(row);*/
-
-  // CREATE TABLE repo_info (
-  //   id INT AUTO_INCREMENT PRIMARY KEY,
-  //   repo_name VARCHAR(255),
-  //   url VARCHAR(255),
-  //   total_score FLOAT,
-  //   ramp_up_score FLOAT,
-  //   correctness_score FLOAT,
-  //   bus_factor FLOAT,
-  //   responsiveness_score FLOAT,
-  //   license_score FLOAT,
-  //   version_score FLOAT,
-  //   adherence_score FLOAT)
-
 
 export async function createMainTable(){
-  //console.log("In create Repo function");
+  console.log("In create Repo function");
   const stmt = `
       CREATE TABLE main_table (
         id INT AUTO_INCREMENT PRIMARY KEY,
@@ -68,8 +50,8 @@ export async function createMainTable(){
 }
 
 
-export async function creatRepoTable(){
-  //console.log("In create Repo function");
+export async function createRepoTable(){
+  console.log("In create Repo Table function");
   const stmt = `
       CREATE TABLE repo_table (
         id INT AUTO_INCREMENT PRIMARY KEY,
@@ -82,8 +64,8 @@ export async function creatRepoTable(){
   console.log('Repo table was successfully created');
 }
 
-export async function creatScoreTable(){
-  //console.log("In create Repo function");
+export async function createScoreTable(){
+  //console.log("In create Score function");
   const stmt = `
       CREATE TABLE score_table (
         id INT AUTO_INCREMENT PRIMARY KEY,
@@ -94,7 +76,7 @@ export async function creatScoreTable(){
         responsiveness_score FLOAT,
         license_score FLOAT,
         version_score FLOAT,
-        adherence_score FLOAT)
+        adherence_score FLOAT
       )
     `;
   await pool.query(stmt);
@@ -102,19 +84,18 @@ export async function creatScoreTable(){
 }
 export async function insertScoreTable(total_score, ramp_up_score, correctness_score, bus_factor, responsiveness_score, license_score, version_score, adherence_score) {
   const stmt = `INSERT INTO score_table (total_score, ramp_up_score, correctness_score, bus_factor, responsiveness_score, license_score, version_score, adherence_score) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
-  const result = await pool.query(stmt, [total_score, ramp_up_score, correctness_score, bus_factor, responsiveness_score, license_score, version_score, adherence_score]);
+  const [result] = await pool.query(stmt, [total_score, ramp_up_score, correctness_score, bus_factor, responsiveness_score, license_score, version_score, adherence_score]);
   return result.insertId;
 }
 export async function insertRepoTable(url, zip, readme) {
   const stmt = `INSERT INTO repo_table (url, zip, readme) VALUES (?, ?, ?)`;
-  const result = await pool.query(stmt, [url, zip, readme]);
+  const [result] = await pool.query(stmt, [url, zip, readme]);
   return result.insertId;
 }
 export async function insertMainTable(name, version, name_tag, repo_id, score_id) {
   const stmt = `INSERT INTO main_table (name, version, name_tag, repo_id, score_id) VALUES (?, ?, ?, ?, ?)`;
   const result = await pool.query(stmt, [name, version, name_tag, repo_id, score_id]);
   console.log(`Inserted into Main Table: ${name}, ${version}, ${name_tag}, ${repo_id}, ${score_id}`);
-
 }
 
 export async function deleteID(name_tag){
@@ -146,10 +127,16 @@ export async function deleteID(name_tag){
   }
 }
 
+// export async function deleteTable(table_name) {
+//     const stmt = `DROP TABLE IF EXISTS ${table_name}`;
+//     await pool.query(stmt);
+//     console.log(`Table ${table_name} has been deleted.`);
+// }
 export async function deleteTable(table_name) {
-    const stmt = `DROP TABLE IF EXISTS ${table_name}`;
-    await pool.query(stmt);
-    console.log(`Table ${table_name} has been deleted.`);
+  console.log("In delete Table function");
+  const stmt = `DROP TABLE IF EXISTS ${table_name}`;
+  await pool.query(stmt);
+  console.log(`Table ${table_name} has been deleted.`);
 }
 
 export async function retrieveRepoTable(name_tag) {
@@ -158,6 +145,7 @@ export async function retrieveRepoTable(name_tag) {
   if (mainRes.length === 0) {
     throw new Error(`No rows found for nameTag: ${name_tag}`);
   }
+  console.log(mainRes);
   const repo_id = mainRes[0].repo_id;
 
   const repoStmt = `SELECT * FROM repo_table WHERE id = ?`;
@@ -194,7 +182,7 @@ export async function retrieveMainTable(name_tag) {
     FROM main_table
     WHERE name_tag = ?
   `;
-  const result = await pool.query(stmt, [name_tag]);
+  const [result] = await pool.query(stmt, [name_tag]);
   return result[0]; // return first row of results
 }
 export async function retrieveAllTables(name_tag) {
@@ -207,117 +195,48 @@ export async function retrieveAllTables(name_tag) {
   console.log(scoreData);
   console.log(MainData);
 }
+/*
 export async function insertALLTable(name, version, name_tag, url, zip,
   readme, total_score,ramp_up_score,correctness_score,bus_factor,responsiveness_score,
   license_score,version_score,adherence_score){
 
   const repo_id =  await insertRepoTable(url, zip, readme);
   const score_id = await insertScoreTable(total_score,ramp_up_score,correctness_score,bus_factor,responsiveness_score,license_score,version_score,adherence_score);
+  console.log(repo_id);
+  console.log(score_id);
   await insertMainTable(name, version, name_tag, repo_id, score_id);
-  const get_repo_query = 'SELECT repo_id, score_id FROM main_table WHERE name_tag = ?';
-}
-
-//deleteTable("main_table");
-deleteTable("main_table");
-deleteTable("repo_table");
-deleteTable("score_table");
-createMainTable();
-creatRepoTable();
-creatScoreTable();
-
-
-//createRepoTable();
-
-
-
-     /*
-        const check_stmt = `
-        SELECT EXISTS(
-        SELECT 1 FROM repo_info WHERE url = :url LIMIT 1
-        )
-        `;
-        const check_result = await pool.query(check_stmt, { url });
-
-        if (check_result && check_result[0] && check_result[0][0]) {
-        throw new Error(`Record with URL ${url} already exists in the database`);
-        }
-        
-
-        // Insert new record into repo_info table
-        const stmt = `
-        INSERT INTO repo_info (repo_name, url, total_score, ramp_up_score, correctness_score,
-        bus_factor, responsiveness_score, license_score, version_score, adherence_score)
-        VALUES (:repo_name, :url, :total_score, :ramp_up_score, :correctness_score,
-        :bus_factor, :responsiveness_score, :license_score, :version_score, :adherence_score)
-        `;
-
-        await pool.query(stmt, {
-        repo_name,
-        url,
-        total_score,
-        ramp_up_score,
-        correctness_score,
-        bus_factor,
-        responsiveness_score,
-        license_score,
-        version_score,
-        adherence_score,
-        });
-        console.log("Data inserted successfully.");
-        */
-
-/*
-export async function insert_repo_data(repo_name, url, total_score, ramp_up_score, correctness_score,
-    bus_factor, responsiveness_score, license_score, version_score, adherence_score) {
-
-        const result = await pool.query(
-          'INSERT INTO repo_info (repo_name, url, total_score, ramp_up_score, correctness_score, bus_factor, responsiveness_score, license_score, version_score, adherence_score) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-          [repo_name, url, total_score, ramp_up_score, correctness_score, bus_factor, responsiveness_score, license_score, version_score, adherence_score]
-        );
-        console.log("DATA was inserted");
-}
-
-async function retrieve_repo_data_url(url) {
-    const result = await pool.query(
-        "SELECT * FROM repo_info WHERE url=:url", { url: url }
-    );
-    if (result && result.length > 0) {
-      return result[0];
-    } else {
-      return null;
-    }
-
-    //return result[0];
-}
-
-
-export async function retrieve_repo_data(){
-  const [result] = await pool.query("SELECT * FROM repo_info");
-  return result;
-}
-
-const output = await retrieve_repo_data();
-console.log(output[0].bus_factor);
-*/
-
-//deleteTable("repo_info");
-//createRepoTable();
-//insert_repo_data("my_repo", "https://github.com/LMAO", 0.8, 0.7, 0.9, 0.6, 0.85, 0.9, 0.75, 0.8);
-//console.log(result);
-/*
-if (result) {
-  console.log('Result:', result);
-} else {
-  console.log(`No repo found with URL: ${url}`);
-}
-const repoData = await retrieve_repo_data();
-if (repoData) {
-  console.log(repoData.rows);
-} else {
-  console.log("No repo data found");
 }*/
-//deleteTable("repo_info");
+export async function insertALLTable(name, version, name_tag, url, zip, readme, total_score, ramp_up_score, correctness_score, bus_factor, responsiveness_score, license_score, version_score, adherence_score) {
+  try {
+    const repo_id = await insertRepoTable(url, zip, readme);
+    console.log(`Repo ID: ${repo_id}`);
+    const score_id = await insertScoreTable(total_score, ramp_up_score, correctness_score, bus_factor, responsiveness_score, license_score, version_score, adherence_score);
+    console.log(`Score ID: ${score_id}`);
+    await insertMainTable(name, version, name_tag, repo_id, score_id);
+  } catch (err) {
+    console.error(err);
+  }
+}
 
-//createZipTable();
-//deleteTable("zipped_table");
+/*
+await deleteTable("main_table");
+await deleteTable("repo_table");
+await deleteTable("score_table");
+await createMainTable();
+await createRepoTable();
+await createScoreTable();
+await insertALLTable("name", "version", "name_tag", "url", "zip",
+  "readme", 0.0,0.1,0.2,0.3,0.4,
+  0.5,0.6,0.7);
+
+await insertALLTable("name1", "version1", "name_tag1", "url1", "zip1",
+  "readme1", 0.0,0.1,0.2,0.3,0.4,
+  0.5,0.6,0.7); 
+await retrieveAllTables("name_tag");*/
+//await deleteID("name_tag");
+//await retrieveAllTables("name_tag");
+//await retrieveAllTables("name_tag");
+
+
+
 
